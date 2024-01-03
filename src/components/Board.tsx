682c79b2b544f8a90b98645e75f9d0ef2792f349
Tyson <100465135+DieMantra@users.noreply.github.com>
2024-01-03 18:21:04 +1100

    fix

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,9 +6,9 @@ export default function Board() {
 	return (
 		<div
 			className={cn(
-				'max-w-[376px] max-h-dvh sm:max-h-[700px] w-full flex flex-col h-full',
+				'max-w-[376px] max-h-svh sm:max-h-[700px] w-full flex flex-col h-full',
 				'relative bg-slate-950 z-10',
-				'rounded-3xl shadow-2xl'
+				'sm:rounded-3xl shadow-2xl'
 			)}
 		>
 			<div className='absolute -z-10 h-full w-full bg-[radial-gradient(#041831_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]'></div>
